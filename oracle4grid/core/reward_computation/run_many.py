e5c88625a78b0ec7a68004549ebb64ca897245cb
mjothy <mario.jothy@artelys.com>
2020-10-26 16:22:55 +0100

    quick fixes for runner

--- oracle4grid/core/reward_computation/run_many.py
+++ oracle4grid/core/reward_computation/run_many.py
@@ -9,7 +9,10 @@ from oracle4grid.core.reward_computation.run_one import run_one
 
 
 def run_all(actions, env, max_iter=1, nb_process=1):
-    all_res = parallel(env, actions, max_iter, nb_process)
+    if nb_process is 1:
+        all_res = serie(env, actions, max_iter)
+    else:
+        all_res = parallel(env, actions, max_iter, nb_process)
     df = make_df_from_res(all_res)
     return df
 
@@ -18,6 +21,15 @@ def make_df_from_res(all_res):
     cols = ["action", "id", "cum_reward", "nb_timesteps", "episode_data"]
     df = pandas.DataFrame(all_res, columns=cols)
     return df
+
+
+def serie(env, actions, max_iter):
+    all_res = []
+    with tqdm(total=len(actions)) as pbar:
+        for action in actions:
+            all_res.append(run_one(action, env, max_iter))
+            pbar.update(1)
+    return all_res
 
 
 def parallel(env, actions, max_iter, nb_process):
