3971d88930b9961d0a40089cb7c74f0edd347197
marota <antoine-marot@live.fr>
2020-12-22 16:52:13 +0100

    compute elapse time and only generate graph visualisation if small enough

--- oracle4grid/core/oracle.py
+++ oracle4grid/core/oracle.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from oracle4grid.core.graph import graph_generator, compute_trajectory, indicators
 from oracle4grid.core.utils.prepare_environment import get_initial_configuration
@@ -13,10 +14,17 @@ def oracle(atomic_actions, env, debug, config, debug_directory=None):
     init_topo_vect, init_line_status = get_initial_configuration(env)
 
     # 1 - Action generation step
+    start_time = time.time()
     actions = combinator.generate(atomic_actions, int(config[MAX_DEPTH]), env, debug, init_topo_vect, init_line_status)
+    elapsed_time = time.time() - start_time
+    print("elapsed_time for action generation is:"+str(elapsed_time))
 
     # 2 - Actions rewards simulation
+    start_time = time.time()
     reward_df = run_many.run_all(actions, env, int(config[MAX_ITER]), int(config[NB_PROCESS]), debug=debug)
+    elapsed_time = time.time() - start_time
+    print("elapsed_time for simulation is:"+str(elapsed_time))
+
     if debug:
         print(reward_df)
         serialize_reward_df(reward_df, debug_directory)
@@ -24,14 +32,20 @@ def oracle(atomic_actions, env, debug, config, debug_directory=None):
         # reward_df = load_serialized_object('reward_df', debug_directory)
 
     # 3 - Graph generation
+    start_time = time.time()
     graph = graph_generator.generate(reward_df, init_topo_vect, init_line_status, int(config[MAX_ITER]), debug=debug)
-    if debug:
-        # serialize(graph, name="graphe", dir=debug_directory)
+    elapsed_time = time.time() - start_time
+    print("elapsed_time for graph creation is:"+str(elapsed_time))
+    if debug and len(graph.nodes)<100:
+        serialize(graph, name="graphe", dir=debug_directory)
         draw_graph(graph, int(config[MAX_ITER]), save=debug_directory)
 
     # 4 - Best path computation (returns actions.npz + a list of atomic action dicts??)
+    start_time = time.time()
     best_path, grid2op_action_path = compute_trajectory.best_path(graph, config["best_path_type"], actions,
                                                                   init_topo_vect, init_line_status, debug=debug)
+    elapsed_time = time.time() - start_time
+    print("elapsed_time for best_path computation is:"+str(elapsed_time))
 
     if debug:
         print(best_path)
