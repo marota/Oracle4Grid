69b75947bdabc118872411d76e59eda33af3b2f4
marota <antoine-marot@live.fr>
2020-12-22 12:05:46 +0100

    changing path in integration_test file - the test folders are already in oracle4grid foler

--- oracle4grid/test/integration_test.py
+++ oracle4grid/test/integration_test.py
@@ -27,7 +27,7 @@ CONFIG = {
 
 class IntegrationTest(unittest.TestCase):
     def test_base_run(self):
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = "000"
         env_dir = "./data/rte_case14_realistic"
         action_path, grid2op_action_path, kpis = load_and_run(env_dir, chronic, file, False, CONFIG)
@@ -35,7 +35,7 @@ class IntegrationTest(unittest.TestCase):
         return 1
 
     def test_best_path_equal(self):
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = "000"
         env_dir = "./data/rte_case14_realistic"
         action_path, grid2op_action_path, indicators = load_and_run(env_dir, chronic, file, False, CONFIG)
@@ -45,7 +45,7 @@ class IntegrationTest(unittest.TestCase):
 
     def test_agent_rewards(self):
         # Compute best path with Oracle
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = 0
         env_dir = "./data/rte_case14_realistic"
         action_path, grid2op_action_path, indicators = load_and_run(env_dir, chronic, file, False, CONFIG)
@@ -75,7 +75,7 @@ class IntegrationTest(unittest.TestCase):
                         "sub-1-0_sub-5-2_line-4-3", "sub-1-1_sub-5-2_line-4-3", "donothing-0"]
 
     def test_actions_combination(self):
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = "000"
         env_dir = "./data/rte_case14_realistic"
         atomic_actions, env, debug_directory = load(env_dir, chronic, file, False)
@@ -88,15 +88,15 @@ class IntegrationTest(unittest.TestCase):
         self.assertListEqual(list(actions), self.expected_actions)
 
     def test_kpi(self):
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = "000"
         env_dir = "./data/rte_case14_realistic"
         action_path, grid2op_action_path, indicators = load_and_run(env_dir, chronic, file, False, CONFIG)
-        expected = pd.read_csv('./oracle4grid/test_resourses/test_kpi.csv', sep=',', index_col=0)
+        expected = pd.read_csv('./test_resourses/test_kpi.csv', sep=',', index_col=0)
         assert_frame_equal(indicators, expected)
 
     def test_reward_df(self):
-        file = "./oracle4grid/ressources/actions/rte_case14_realistic/test_unitary_actions.json"
+        file = "./ressources/actions/rte_case14_realistic/test_unitary_actions.json"
         chronic = "000"
         env_dir = "./data/rte_case14_realistic"
         atomic_actions, env, debug_directory = load(env_dir, chronic, file, False)
@@ -108,7 +108,7 @@ class IntegrationTest(unittest.TestCase):
         # 2 - Actions rewards simulation
         reward_df = run_many.run_all(actions, env, int(CONFIG[MAX_ITER]), int(CONFIG[NB_PROCESS]), debug=False)
         reward_df["action"] = reward_df["action"].astype(str)
-        expected = pd.read_csv('./oracle4grid/test_resourses/test_reward.csv', sep=',', index_col=0)
+        expected = pd.read_csv('./test_resourses/test_reward.csv', sep=',', index_col=0)
         assert_frame_equal(reward_df, expected)
 
 
