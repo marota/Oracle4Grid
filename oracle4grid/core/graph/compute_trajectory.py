f3c13afdacffdca3f7b86d96359fe74618fbe714
mjothy <mario.jothy@artelys.com>
2021-01-18 13:57:37 +0100

    small refactor

--- oracle4grid/core/graph/compute_trajectory.py
+++ oracle4grid/core/graph/compute_trajectory.py
@@ -35,16 +35,8 @@ def best_path_no_overload(graph, best_path_type, actions, init_topo_vect, init_l
     for node in nodes:
         if node.endswith("_overload"):
             graph_no_overload.remove(node)
-    if best_path_type == SHORTEST:
-        path = nx.bellman_ford_path(graph_no_overload, 'init', 'end')  # shortest_path(G)
-    elif best_path_type == LONGEST:
-        path = nx.dag_longest_path(graph_no_overload)  # Longest path
-    # Add more treatment here
 
-    # Traduce path in terms of OracleActions
-    action_path = return_action_path(path, actions, debug=debug)
-    grid2op_action_path = get_grid2op_action_path(action_path, init_topo_vect, init_line_status)
-    return action_path, grid2op_action_path
+    return best_path(graph_no_overload, best_path_type, actions, init_topo_vect, init_line_status, debug)
 
 
 def return_action_path(path, actions, debug=False):
