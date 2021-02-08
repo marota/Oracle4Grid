5cb584a9bde93c86df1e2cd4b0bd05f1f926b12a
mjothy <mario.jothy@artelys.com>
2021-02-08 14:02:40 +0100

    cleanup

--- oracle4grid/core/graph/graph_generator.py
+++ oracle4grid/core/graph/graph_generator.py
@@ -13,7 +13,7 @@ def generate(reward_df, init_topo_vect, init_line_status, max_iter=None, debug=F
     if debug:
         print('\n')
         print("============== 3 - Graph generation ==============")
-        
+
     # Parameters and DataFrame preprocessing
     reward_df = preprocessing(reward_df, max_iter, explicit_node_names=debug)
     actions = reward_df['action'].unique()
@@ -234,23 +234,7 @@ def build_transition_graph(reachable_topologies, ordered_names, reward_df, max_i
 
 
 def post_processing_rewards(graph, reward_df):
-
-    #graph1=graph.copy()
-
-    reward_df["node_name"]=reward_df['name'].astype(str) + '_t' + reward_df['timestep'].astype(str)
-    v_attributes=reward_df[['node_name','overload_reward']].set_index('node_name').to_dict('index')
+    reward_df["node_name"] = reward_df['name'].astype(str) + '_t' + reward_df['timestep'].astype(str)
+    v_attributes = reward_df[['node_name', 'overload_reward']].set_index('node_name').to_dict('index')
     nx.set_node_attributes(graph, v_attributes)
-#
-
-    #for node in graph.nodes:
-    #    if node is "init" or node is "end":
-    #        continue
-    #    action = node.split("_t")[0]
-    #    timestep = node.split("_t")[1]
-    #    line = reward_df.loc[(reward_df['name'].astype(str) == str(action)) & (reward_df['timestep'] == int(timestep))]
-    #    graph.nodes[node]["overload_reward"] = line.iloc[0]["overload_reward"]
-#
-    #(nx.get_node_attributes(graph, 'overload_reward') ==nx.get_node_attributes(graph1, 'overload_reward'))
-
     return graph
-
