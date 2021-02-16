276fa69e8301803d8b3e16fba2050518909199bb
NMegel <nico.megel@gmail.com>
2021-02-16 12:15:13 +0100

    temporary fix when simulation diverges - rewards and other rewards are set at the same convention with NaN

--- oracle4grid/core/reward_computation/run_many.py
+++ oracle4grid/core/reward_computation/run_many.py
@@ -22,12 +22,24 @@ def run_all(actions, env, max_iter=1, nb_process=1, debug=False,agent_seed=None,
 def make_df_from_res(all_res):
     cols = ["action", "timestep", "reward", "overload_reward"]
     data = []
+
     for run in all_res:
+        # Temporary fix in case there is divergence - rewards and other_rewards should be under the same convention (length, NaN)
+        run = check_other_rewards(run)
         for t in range(run.rewards.shape[0]):
             data.append({"action": run.action, "timestep": t, "reward": run.rewards[t], "overload_reward": run.other_rewards[t]["overload_reward"]})
     df = pandas.DataFrame(data, columns=cols)
     return df
 
+def check_other_rewards(run):
+    other_rewards = run.other_rewards.copy()
+    ref_dict = other_rewards[0]
+    n_other_rewards = len(other_rewards)
+    if n_other_rewards < run.max_ts:
+        ref_dict_nan = {key: float('nan') for key in ref_dict.keys()}
+        other_rewards += [ref_dict_nan for i in range(run.max_ts-n_other_rewards)]
+        run.other_rewards = other_rewards
+    return run
 
 def serie(env, actions, max_iter,agent_seed=None,env_seed=None):
     all_res = []
