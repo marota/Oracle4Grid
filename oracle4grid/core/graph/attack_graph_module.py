177a2e280d5acadc5782dd8ce4de9bd4c04170c1
mjothy <mario.jothy@artelys.com>
2021-05-04 15:59:59 +0200

    refactor of graph generation to allow easy swap between filtering methods

--- oracle4grid/core/graph/attack_graph_module.py
+++ oracle4grid/core/graph/attack_graph_module.py
@@ -5,62 +5,6 @@ import itertools
 import networkx as nx
 
 
-def filter_attacked_nodes(edges_or, edges_ex, edges_weights, reward_df):
-    # attacks_per_topo_df = reward_df[['name', 'attack_id']].drop_duplicates()
-    # Transform DF to dict
-    attacks_per_topo = reward_df.dropna()[['timestep', 'name', 'attack_id']].to_dict("list")
-    mask = [is_masked(edge_or, edge_ex, attacks_per_topo) for (edge_or, edge_ex) in zip(edges_or, edges_ex)]
-    print("Compressing attack lists...")
-    start_time = time.time()
-    new_or = list(compress(edges_or, mask))
-    new_ex = list(compress(edges_ex, mask))
-    new_weights = list(compress(edges_weights, mask))
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-    print("number of edges removes because of inconsistency in attacks : " + str(len(edges_or) - len(new_or)))
-    return new_or, new_ex, new_weights
-
-def filter_attack_edges(reachable_topologies,topo_ordered_names,reward_df,max_iter,n_init,n_end):
-    """
-    Other possible function to filter edges that don't share the attack id
-    """
-    print("Removing attack edges")
-    # To filter graph edges whose nodes don't share the same attack (if attack exist)
-    attacks_table = reward_df[['timestep', 'attack_id', 'name']].pivot(index='timestep', columns='name',
-                                                                       values='attack_id')
-    attacks_table = attacks_table.reset_index(drop=True).rename_axis(None,
-                                                                     axis=1)  # to have a proper data table without parasite names
-
-    edges_attack_diff = list(
-        itertools.chain(*[attacks_table[reachable_topologies[i]][1:max_iter].reset_index(drop=True).
-                        sub(attacks_table[topo_ordered_names[i]][0:max_iter - 1], axis=0).values.flatten(order='F')
-                          for i in range(len(topo_ordered_names))] ))
-
-
-
-    edges_attack_filtered_after_init_befor_end = [False if (np.isnan(edges_attack_diff[i]) or edges_attack_diff[i] == 0) else True for i in
-                              range(len(edges_attack_diff))]
-    #add filter for init and end node
-    edges_attack_filtered_init = [False for i in range(n_init)]
-    edges_attack_filtered_end=[False for i in range(n_end)]
-
-    #number of edges normally filtered on the test case,by looking at reachable topology for each topology and attacks per topology at timestep 400
-    # 1*48+1*48+1*48+1*48+1*48+1*48+1*48+1*48=8*48=384 over test case, for 2nd window
-    edges_attack_filtered = edges_attack_filtered_init + edges_attack_filtered_after_init_befor_end + edges_attack_filtered_end
-    print("number of edges removes because of inconsistency in attacks : " + str(np.sum(edges_attack_filtered)))
-    return edges_attack_filtered
-
-
-def is_masked(edge_or, edge_ex, attacks_per_topo):
-    if edge_or is "init" or edge_ex is "end" or edge_or is "end" or edge_ex is "init":
-        return True
-    # OR data
-    or_attack = get_attack_from_edge(edge_or, attacks_per_topo)
-    # EX data
-    ex_attack = get_attack_from_edge(edge_ex, attacks_per_topo)
-    return or_attack is None or ex_attack is None or or_attack == ex_attack
-
-
 def get_info_from_edge(edge):
     split_1 = edge.split("_t")
     topo = split_1[0]
@@ -70,19 +14,6 @@ def get_info_from_edge(edge):
     if len(split_2) == 2:
         attack = split_2[1]
     return topo, timestep, attack
-
-def get_attack_from_edge(edge, attacks_per_topo):
-    topo, timestep, attack = get_info_from_edge(edge)
-    t_indexes = [i for i, x in enumerate(attacks_per_topo['timestep']) if x == timestep]
-    #name_indexes = [i for i, x in enumerate(attacks_per_topo['name']) if x == int(topo)]#x=topo
-    name_indexes = [i for i, x in enumerate(attacks_per_topo['name']) if str(x) == str(topo)]
-    final_indexes = set(t_indexes).intersection(name_indexes)
-    if len(final_indexes) == 1:
-        return attacks_per_topo['attack_id'][final_indexes.pop()]
-    elif len(final_indexes) == 0:
-        return None
-    else:
-        raise IndexError("too many values to unpack : " + len(final_indexes))
 
 
 def get_windows_from_df(reward_df):
--- oracle4grid/core/graph/graph_generator.py
+++ oracle4grid/core/graph/graph_generator.py
@@ -137,18 +137,20 @@ def build_transition_graph(reachable_topologies, ordered_names, reward_df, max_i
     :return: networkx.digraph with all possible transitions and their rewards at each timestep
     """
 
+    graph_time = time.time()
+
     # First edges: symbolic init node and reachable first actions
     edges_or, edges_ex, edges_weights = create_init_nodes(reachable_topologies_from_init, reward_df)
     n_init = len(edges_weights)
 
+    # Find and create all possible edges
     # Reachable transitions for each timestep and associated rewards
     start_time = time.time()
     inverted = get_inverted_reachable_topologies(ordered_names, reachable_topologies)
-    reward_df_with_edges = reward_df
-    list_topo = reward_df_with_edges['name']
-    list_attack = reward_df_with_edges['attack_id']
-    list_timestep = reward_df_with_edges["timestep"]
-    list_weights = reward_df_with_edges["reward"]
+    list_topo = reward_df['name']
+    list_attack = reward_df['attack_id']
+    list_timestep = reward_df["timestep"]
+    list_weights = reward_df["reward"]
     prevAtk = None
     for (topo, attack, t, reward) in zip(list_topo, list_attack, list_timestep, list_weights):
         if t == 0:
@@ -163,28 +165,23 @@ def build_transition_graph(reachable_topologies, ordered_names, reward_df, max_i
     uniques_or = set([node for node in edges_or if '_atk' in node])
     uniques_ex = set([node for node in edges_ex if '_atk' in node])
     to_dump = uniques_or.difference(uniques_ex)
-    print("number of nodes to dump :" + str(len(to_dump)))
-
-    # Optional filtering
-    indexes_to_dump = [i for i in range(len(edges_or)) if edges_or[i] in to_dump]
-    edges_ex_filtered = [i for j, i in enumerate(edges_ex) if j not in indexes_to_dump]
-    edges_or_filtered = [i for j, i in enumerate(edges_or) if j not in indexes_to_dump]
-    edges_weights_filtered = [i for j, i in enumerate(edges_weights) if j not in indexes_to_dump]
-
     elapsed_time = time.time() - start_time
+    print("number of nodes to dump :" + str(len(to_dump)))
     print(elapsed_time)
+
+    # filtering (option 1)
+    edges_ex, edges_or, edges_weights = filter_nodes_from_python_list(edges_or, edges_ex, edges_weights, to_dump)
 
     # Symbolic end node
     edges_ex_t = [str(name) + '_t' + str(max_iter - 1) for name in ordered_names]
     or_end, ex_end, weights_end = create_end_nodes(edges_ex_t, fake_reward=END_NODE_REWARD)
-    edges_or_filtered += or_end
-    edges_ex_filtered += ex_end
-    edges_weights_filtered += weights_end
+    edges_or += or_end
+    edges_ex += ex_end
+    edges_weights += weights_end
     n_end = len(weights_end)
 
     # Finally create graph object from DataFrame
-
-    edge_df = pd.DataFrame({'or': edges_or_filtered, 'ex': edges_ex_filtered, 'weight': edges_weights_filtered})
+    edge_df = pd.DataFrame({'or': edges_or, 'ex': edges_ex, 'weight': edges_weights})
     edge_df = edge_df.dropna()
 
     print("edge_df done - creating graph")
@@ -197,11 +194,36 @@ def build_transition_graph(reachable_topologies, ordered_names, reward_df, max_i
 
     graph = post_processing_rewards(graph, reward_df)
 
-    print("removing nodes created for attack but useless")
-    #graph.remove_nodes_from(to_dump)
-    print("graph created")
+    # Node removal (option 2)
+    # graph = remove_nodes_from_graph_networkx(graph, to_dump)
 
+    print("graph created, total time :")
+    elapsed_time = time.time() - graph_time
+    print(elapsed_time)
     return graph
+
+
+def remove_nodes_from_graph_networkx(graph, to_dump):
+    print("removing nodes created for attack but useless")
+    start_time = time.time()
+    graph.remove_nodes_from(to_dump)
+    elapsed_time = time.time() - start_time
+    print("filtering time :")
+    print(elapsed_time)
+    return graph
+
+
+def filter_nodes_from_python_list(edges_or, edges_ex, edges_weights, to_dump):
+    start_time = time.time()
+    indexes_to_dump = [i for i in range(len(edges_or)) if edges_or[i] in to_dump]
+    edges_ex_filtered = [i for j, i in enumerate(edges_ex) if j not in indexes_to_dump]
+    edges_or_filtered = [i for j, i in enumerate(edges_or) if j not in indexes_to_dump]
+    edges_weights_filtered = [i for j, i in enumerate(edges_weights) if j not in indexes_to_dump]
+
+    elapsed_time = time.time() - start_time
+    print("filtering time :")
+    print(elapsed_time)
+    return edges_ex_filtered, edges_or_filtered, edges_weights_filtered
 
 
 def post_processing_rewards(graph, reward_df):
